0cded122b9a08c9614a5a98384d8ddeb625bde45
lnksys <74170089+lnksys@users.noreply.github.com>
2021-09-12 13:34:26 +0530

    initial commit

--- src/app/recipies/recipe-list/recipe-list.component.ts
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  recipies: Recipe[] = [
+    new Recipe(
+      "A test recipe",
+      "This is a test",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwDO-nnMlM-ws_rFGJIc4Z634gEr9twz-KZiSOTYFLQlqL2IJvLZhEqm3AUN0asLwQF14&usqp=CAU"
+    ),
+    new Recipe(
+      "A test recipe",
+      "This is a test",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwDO-nnMlM-ws_rFGJIc4Z634gEr9twz-KZiSOTYFLQlqL2IJvLZhEqm3AUN0asLwQF14&usqp=CAU"
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

5565cdd644fddf2c81a420595fe62a3a096e3f0a
lnksys <74170089+lnksys@users.noreply.github.com>
2021-09-22 17:57:13 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'recipe-app';
+  loadedFeature = "recipe";
+  title = "recipe-app";
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
